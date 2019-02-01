7e14cd3e9fcdf3434e4894dffcc2319fba897676
Mateusz Jurzyna <jurzyna@gmail.com>
2019-02-01 12:54:54 +0100

    basic router structure

---
+++ src/Containers/NewsDetails.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class NewsDetails extends Component {
+    render() {
+        return (
+            <h2>News Details</h2>
+        );
+    }
+}
+
+export default NewsDetails;
---
+++ src/Containers/NewsList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class NewsList extends Component {
+    render() {
+        return (
+            <h2>News List</h2>
+        );
+    }
+}
+
+export default NewsList;
