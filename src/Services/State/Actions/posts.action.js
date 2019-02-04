b890bfa705610959f5c78c9183e61af67598a314
Mateusz Jurzyna <jurzyna@gmail.com>
2019-02-04 09:27:27 +0100

    feat: adding offline solution

--- src/Services/State/Actions/posts.action.js
+++ src/Services/State/Actions/posts.action.js
@@ -3,6 +3,7 @@
  */
 import {fetchAsync} from "../../helpers";
 import {baseUrl, newsApiOptions} from "../../Config/newsApiConfig";
+import {posts} from "../../../JSON/posts";
 
 export const GET_POSTS = 'get posts';
 export const GET_POSTS_ERROR = 'get posts';
@@ -23,9 +24,19 @@ export function getPostsThunk() {
     return dispatch => {
         dispatch(getPosts());
         fetchAsync(baseUrl, 'top-headlines', newsApiOptions)
-            .then(data =>  dispatch(getPostsCommit(data.articles)))
+            .then(data =>  {
+                dispatch(getPostsCommit(data.articles));
+                // save in local storage for offline use
+                localStorage.setItem('posts', JSON.stringify(data.articles))
+            })
             .catch(reason => {
-                dispatch(getPostsError(reason))
+                dispatch(getPostsError(reason));
+                const dataCopy =  localStorage.getItem('posts');
+                // read from storage when app is offline or error occurs
+                if (dataCopy){
+                    dispatch(getPostsCommit(JSON.parse(dataCopy)));
+                }
+
             });
 
     }
