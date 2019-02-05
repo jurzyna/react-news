ecbc81d0ef1c485fbcddd59fa4df579d3fdeee35
Mateusz Jurzyna <jurzyna@gmail.com>
2019-02-05 22:26:48 +0100

    transition: simple rotate pose transition

--- src/Containers/Header.js
+++ src/Containers/Header.js
@@ -1,27 +1,34 @@
 import React from "react";
-import {HeaderWrap, Logo, LogoWrap, MenuIcon} from "./Header.styled";
+import {HeaderWrap, Logo, LogoWrap} from "./Header.styled";
 import LogoFile from '../Assets/Images/logo_large.png';
 import Icon from "../Components/Icon";
 import posed from 'react-pose';
 
-const Box = posed.div({
-    hidden: {opacity: 0},
-    visible: {opacity: 1}
+const MenuIcon = posed.div({
+    closed: {
+        rotate: 0,
+        transition: { duration: 500 }
+    },
+    open: {
+        rotate: 180,
+        transition: { duration: 500 }
+    }
 });
 
 class Header extends React.Component {
     state = {isOpen: false};
 
-
     toggleMenu = () => {
-        // this.setState({ isOpen: !this.state.isOpen });
+        this.setState(state => ({
+            isOpen: !state.isOpen
+        }));
     };
 
     render() {
         const {isOpen} = this.state;
         return (
             <HeaderWrap>
-                <MenuIcon onClick={this.toggleMenu()} pose={isOpen ? 'open' : 'close'}>Icon</MenuIcon>
+                <MenuIcon pose={isOpen ? 'open' : 'closed'} onClick={this.toggleMenu}>Box</MenuIcon>
                 <LogoWrap>
                     <Logo src={LogoFile}/>
                 </LogoWrap>
