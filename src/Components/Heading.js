a86634b141500670c179ae9fbfc5b920893fe951
Mateusz Jurzyna <jurzyna@gmail.com>
2019-02-05 09:12:07 +0100

    style: set typography

--- src/Components/Heading.js
+++ src/Components/Heading.js
@@ -3,15 +3,16 @@ import { rem } from 'polished';
 
 // Define new const with bold style
 const headingStyle = css`
-  font-weight: 600;
   text-align: left;
-  line-height: 1.2;
+  line-height: 1.4;
   color: rgba(0,0,0,.84);
 `;
 
 export const H1 = styled.h1`
   margin: ${rem(10)} 0 0;
   font-size: ${rem(21)};
+  font-style: italic;
+  font-weight: 900;
   ${headingStyle};
 `;
 
@@ -22,7 +23,8 @@ export const H2 = styled.h2`
 
 export const H3 = styled.h3`
   margin: 0;
-  font-size: ${rem(16)};
+  font-family: 'Libre Franklin', sans-serif;
+  font-size: ${rem(13)};
   text-transform: uppercase;
   ${headingStyle};
 `;
