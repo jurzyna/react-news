d4562550e3b7edf15be3b362b6904e8dbdacc7ce
Mateusz Jurzyna <jurzyna@gmail.com>
2019-02-06 17:40:07 +0100

    feat: added router props

---
+++ src/Components/Header.js
@@ -0,0 +1,47 @@
+import React from "react";
+import ReactRouterPropTypes from 'react-router-prop-types';
+
+import {
+    HamburgerMenu,
+    HamburgerMenuBottom,
+    HamburgerMenuMiddle,
+    HamburgerMenuTop,
+    HeaderWrap,
+    Logo,
+    LogoWrap
+} from "./Header.styled";
+import LogoFile from '../Assets/Images/logo_large.png';
+import Icon from "./Icon";
+
+class Header extends React.Component {
+
+    toggleMenu = () => {
+        if (!this.props.match.isExact) {
+            this.props.history.push('/');
+        }
+    };
+
+    render() {
+        return (
+            <HeaderWrap>
+                <HamburgerMenu pose={this.props.match.isExact ? 'closed' : 'open'} onClick={this.toggleMenu}>
+                    <HamburgerMenuTop/>
+                    <HamburgerMenuMiddle/>
+                    <HamburgerMenuBottom/>
+                </HamburgerMenu>
+                <LogoWrap>
+                    <Logo src={LogoFile}/>
+                </LogoWrap>
+                <Icon icon="search"/>
+            </HeaderWrap>
+        )
+    }
+}
+
+export default Header;
+
+// TODO add prop types for history and match
+Header.propTypes = {
+    history: ReactRouterPropTypes.history.isRequired,
+    match: ReactRouterPropTypes.match.isRequired,
+};
